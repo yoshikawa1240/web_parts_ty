ba3b91d96c4eb4d83bfa8aaac61f55ff7c68e4ca
yoshikawa1240 <taka.scholes18@gmail.com>
2021-09-12 08:18:57 +0900

    fix contents_007
    コンテンツ＿007のカテゴリーの余白を修正

--- contents/007/index.php
+++ contents/007/index.php
@@ -1,33 +1,20 @@
-<!DOCTYPE html>
-<html lang="ja">
+<link rel="stylesheet" href="<?php echo $home_url; ?>/web_parts_ty/contents/007/css/style.min.css" );>
+<link rel="stylesheet" href="<?php echo $home_url; ?>/web_parts_ty/contents/007/css/reset.min.css" );>
+<!-- FontAwesome -->
+<link href="https://use.fontawesome.com/releases/v5.6.1/css/all.css" rel="stylesheet">
 
-<head>
-  <meta charset="UTF-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <title>cont_parts_y_007</title>
-  <link rel="stylesheet" href="./css/reset.css">
-  <link rel="stylesheet" href="./css/style.css">
-  <!-- FontAwesome -->
-  <link href="https://use.fontawesome.com/releases/v5.6.1/css/all.css" rel="stylesheet">
-  <!-- GoogleFont -->
-  <link rel="preconnect" href="https://fonts.gstatic.com">
-  <link href="https://fonts.googleapis.com/css2?family=Noto+Serif+JP:wght@200;400;500;700;900&display=swap"
-    rel="stylesheet">
-</head>
-
-<body>
   <!-------------- 
-    cont_parts_y_007
+    cont_ty_007
   ---------------->
-  <article class="cont_parts_y_007">
-    <div class="conts_wrapper">
+  <article class="cont_ty_007">
+    <div class="cont_ty_007_wrapper">
       <section>
         <a href="">
           <div class="image">
             <img src="https://cdn.pixabay.com/photo/2014/08/15/11/29/beach-418742_1280.jpg" alt="">
             <div class="category">カテゴリー</div>
           </div>
-          <div class="cont_wrapper">
+          <div class="outer">
             <div class="inner">
               <h3>タイトルタイトルタイトルタイトルタイトルタイトルタイトルタイトルタイトルタイトルタイトル</h3>
               <span>20201.01.01</span>
@@ -41,7 +28,7 @@
             <img src="https://cdn.pixabay.com/photo/2014/08/15/11/29/beach-418742_1280.jpg" alt="">
             <div class="category">カテゴリー</div>
           </div>
-          <div class="cont_wrapper">
+          <div class="outer">
             <div class="inner">
               <h3>タイトルタイトルタイトルタイトルタイトルタイトルタイトルタイトルタイトルタイトルタイトル</h3>
               <span>20201.01.01</span>
@@ -55,7 +42,7 @@
             <img src="https://cdn.pixabay.com/photo/2014/08/15/11/29/beach-418742_1280.jpg" alt="">
             <div class="category">カテゴリー</div>
           </div>
-          <div class="cont_wrapper">
+          <div class="outer">
             <div class="inner">
               <h3>タイトルタイトルタイトルタイトルタイトルタイトルタイトルタイトルタイトルタイトルタイトル</h3>
               <span>20201.01.01</span>
@@ -69,7 +56,7 @@
             <img src="https://cdn.pixabay.com/photo/2014/08/15/11/29/beach-418742_1280.jpg" alt="">
             <div class="category">カテゴリー</div>
           </div>
-          <div class="cont_wrapper">
+          <div class="outer">
             <div class="inner">
               <h3>タイトルタイトルタイトルタイトルタイトルタイトルタイトルタイトルタイトルタイトルタイトル</h3>
               <span>20201.01.01</span>
@@ -79,7 +66,3 @@
       </section>
     </div>
   </article>
-
-</body>
-
-</html>
