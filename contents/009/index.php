53ad7bc8347bf1b3b0ea0424f9b2a0072b852e36
yoshikawa1240 <taka.scholes18@gmail.com>
2021-09-12 09:30:06 +0900

    fix contents_009
    コンテンツ＿009の修正

--- contents/009/index.php
+++ contents/009/index.php
@@ -1,93 +1,63 @@
-<!DOCTYPE html>
-<html lang="ja">
+<link rel="stylesheet" href="<?php echo $home_url; ?>/web_parts_ty/contents/009/css/reset.min.css" );>
+<link rel="stylesheet" href="<?php echo $home_url; ?>/web_parts_ty/contents/009/css/style.min.css" );>
 
-<head>
-  <meta charset="UTF-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <title>cont_parts_y_009</title>
-  <link rel="stylesheet" href="./css/reset.css">
-  <link rel="stylesheet" href="./css/style.css">
-  <!-- FontAwesome -->
-  <link href="https://use.fontawesome.com/releases/v5.6.1/css/all.css" rel="stylesheet">
-  <!-- GoogleFont -->
-  <link rel="preconnect" href="https://fonts.gstatic.com">
-  <link href="https://fonts.googleapis.com/css2?family=Noto+Serif+JP:wght@200;400;500;700;900&display=swap"
-    rel="stylesheet">
-</head>
-
-<body>
   <!-------------- 
-    cont_parts_y_009
+    cont_ty_009
   ---------------->
-  <article class="cont_parts_y_009">
+  <article class="cont_ty_009">
     <section>
       <a href="">
         <div class="image"><img src="https://cdn.pixabay.com/photo/2016/11/29/07/34/beach-1868130_1280.jpg" alt=""></div>
-        <h3>ここにタイトルを入力</h3>
+        <h2>ここにタイトルを入力</h2>
         <p>ここに説明を入力</p>
       </a>
-      <div class="cont">
-
+      <div class="outer">
         <div class="inner">
-          <h4>タイトルをここに入力</h4>
-          <p>
-            ここにテキストを入力ここにテキストを入力ここにテキストを入力ここにテキストを入力ここにテキストを入力ここにテキストを入力ここにテキストを入力ここにテキストを入力ここにテキストを入力ここにテキストを入力ここにテキストを入力ここにテキストを入力
-          </p>
+          <h3>タイトルをここに入力</h3>
+          <p>ここにテキストを入力ここにテキストを入力ここにテキストを入力ここにテキストを入力ここにテキストを入力ここにテキストを入力ここにテキストを入力ここにテキストを入力ここにテキストを入力ここにテキストを入力ここにテキストを入力ここにテキストを入力</p>
         </div>
       </div>
     </section>
     <section>
       <a href="">
         <div class="image"><img src="https://cdn.pixabay.com/photo/2016/11/29/07/34/beach-1868130_1280.jpg" alt=""></div>
-        <h3>ここにタイトルを入力</h3>
+        <h2>ここにタイトルを入力</h2>
         <p>ここに説明を入力</p>
       </a>
-      <div class="cont">
-
+      <div class="outer">
         <div class="inner">
-          <h4>タイトルをここに入力</h4>
-          <p>
-            ここにテキストを入力ここにテキストを入力ここにテキストを入力ここにテキストを入力ここにテキストを入力ここにテキストを入力ここにテキストを入力ここにテキストを入力ここにテキストを入力ここにテキストを入力ここにテキストを入力ここにテキストを入力
-          </p>
+          <h3>タイトルをここに入力</h3>
+          <p>ここにテキストを入力ここにテキストを入力ここにテキストを入力ここにテキストを入力ここにテキストを入力ここにテキストを入力ここにテキストを入力ここにテキストを入力ここにテキストを入力ここにテキストを入力ここにテキストを入力ここにテキストを入力</p>
         </div>
       </div>
     </section>
     <section>
       <a href="">
         <div class="image"><img src="https://cdn.pixabay.com/photo/2016/11/29/07/34/beach-1868130_1280.jpg" alt=""></div>
-        <h3>ここにタイトルを入力</h3>
+        <h2>ここにタイトルを入力</h2>
         <p>ここに説明を入力</p>
       </a>
-      <div class="cont">
-
+      <div class="outer">
         <div class="inner">
-          <h4>タイトルをここに入力</h4>
-          <p>
-            ここにテキストを入力ここにテキストを入力ここにテキストを入力ここにテキストを入力ここにテキストを入力ここにテキストを入力ここにテキストを入力ここにテキストを入力ここにテキストを入力ここにテキストを入力ここにテキストを入力ここにテキストを入力
-          </p>
+          <h3>タイトルをここに入力</h3>
+          <p>ここにテキストを入力ここにテキストを入力ここにテキストを入力ここにテキストを入力ここにテキストを入力ここにテキストを入力ここにテキストを入力ここにテキストを入力ここにテキストを入力ここにテキストを入力ここにテキストを入力ここにテキストを入力</p>
         </div>
       </div>
     </section>
     <section>
       <a href="">
         <div class="image"><img src="https://cdn.pixabay.com/photo/2016/11/29/07/34/beach-1868130_1280.jpg" alt=""></div>
-        <h3>ここにタイトルを入力</h3>
+        <h2>ここにタイトルを入力</h2>
         <p>ここに説明を入力</p>
       </a>
-      <div class="cont">
-
+      <div class="outer">
         <div class="inner">
-          <h4>タイトルをここに入力</h4>
-          <p>
-            ここにテキストを入力ここにテキストを入力ここにテキストを入力ここにテキストを入力ここにテキストを入力ここにテキストを入力ここにテキストを入力ここにテキストを入力ここにテキストを入力ここにテキストを入力ここにテキストを入力ここにテキストを入力
-          </p>
+          <h3>タイトルをここに入力</h3>
+          <p>ここにテキストを入力ここにテキストを入力ここにテキストを入力ここにテキストを入力ここにテキストを入力ここにテキストを入力ここにテキストを入力ここにテキストを入力ここにテキストを入力ここにテキストを入力ここにテキストを入力ここにテキストを入力</p>
         </div>
       </div>
     </section>
   </article>  
-</body>
 
 <!-- 参考サイト
 https://demo.tcd-theme.com/tcd080/ -->
-
-</html>
