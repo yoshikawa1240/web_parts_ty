229c2da2ccd6485d7cf3e8539ea0963538a302db
yoshikawa1240 <taka.scholes18@gmail.com>
2021-09-12 09:01:47 +0900

    fix contents_008
    コンテンツ＿008の修正

--- contents/008/index.php
+++ contents/008/index.php
@@ -1,35 +1,14 @@
-<!DOCTYPE html>
-<html lang="ja">
+<link rel="stylesheet" href="<?php echo $home_url; ?>/web_parts_ty/contents/008/css/reset.min.css" );>
+<link rel="stylesheet" href="<?php echo $home_url; ?>/web_parts_ty/contents/008/css/style.min.css" );>
 
-<head>
-  <meta charset="UTF-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <title>cont_parts_y_008</title>
-  <link rel="stylesheet" href="./css/reset.css">
-  <link rel="stylesheet" href="./css/style.css">
-  <!-- FontAwesome -->
-  <link href="https://use.fontawesome.com/releases/v5.6.1/css/all.css" rel="stylesheet">
-  <!-- GoogleFont -->
-  <link rel="preconnect" href="https://fonts.gstatic.com">
-  <link href="https://fonts.googleapis.com/css2?family=Noto+Serif+JP:wght@200;400;500;700;900&display=swap"
-    rel="stylesheet">
-</head>
-
-<body>
   <!-------------- 
-    cont_parts_y_008
+    cont_ty_008
   ---------------->
-  <article class="cont_parts_y_008">
+  <article class="cont_ty_008">
     <div class="wrapper">
-      <div class="top">
-        <h2>タイトルタイトルタイトルタイトルタイトルタイトル</h2>
-        <div class="text">
-          <p>テキストテキストテキストテキストテキストテキストテキストテキストテキストテキスト</p>
-          <p>テキストテキストテキストテキストテキストテキストテキストテキストテキストテキスト</p>
-          <p>テキストテキストテキストテキストテキストテキストテキストテキストテキストテキスト</p>
-        </div>
-      </div>
-      <div class="conts">
+      <h2>タイトルタイトルタイトルタイトル</h2>
+      <p>テキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキスト</p>
+      <section>
         <a href="">
           <div class="image"><img src="https://cdn.pixabay.com/photo/2014/08/15/11/29/beach-418742_1280.jpg" alt="">
           </div>
@@ -78,11 +57,7 @@
             <p>テキストテキストテキストテキストテキストテキストテキストテキストテキストテキスト</p>
           </div>
         </a>
-      </div>
-      <div class="link"><a href="">リンクリンク</a></div>
+      </section>
+      <a href="" class="link">リンクリンク</a>
     </div>
   </article>
-
-</body>
-
-</html>
