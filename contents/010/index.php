673d70d1f859f4658a1482b12ed5074310037cd6
yoshikawa1240 <taka.scholes18@gmail.com>
2021-09-12 09:38:50 +0900

    fix contents_010
    コンテンツ＿010の修正

--- contents/010/index.php
+++ contents/010/index.php
@@ -1,52 +1,34 @@
-<!DOCTYPE html>
-<html lang="ja">
+<link rel="stylesheet" href="<?php echo $home_url; ?>/web_parts_ty/contents/010/css/reset.min.css" );>
+<link rel="stylesheet" href="<?php echo $home_url; ?>/web_parts_ty/contents/010/css/style.min.css" );>
 
-<head>
-  <meta charset="UTF-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <title>cont_parts_y_010</title>
-  <link rel="stylesheet" href="./css/reset.css">
-  <link rel="stylesheet" href="./css/style.css">
-  <!-- FontAwesome -->
-  <link href="https://use.fontawesome.com/releases/v5.6.1/css/all.css" rel="stylesheet">
-  <!-- GoogleFont -->
-  <link rel="preconnect" href="https://fonts.gstatic.com">
-  <link href="https://fonts.googleapis.com/css2?family=Noto+Serif+JP:wght@200;400;500;700;900&display=swap"
-    rel="stylesheet">
-</head>
-
-<body>
   <!-------------- 
-    cont_parts_y_010
+    cont_ty_010
   ---------------->
-  <article class="cont_parts_y_010">
+  <article class="cont_ty_010">
     <div class="wrapper">
       <section>
         <a href="">
           <div class="image"><img src="https://cdn.pixabay.com/photo/2017/08/01/20/52/people-2567915_1280.jpg" alt=""></div>
-          <h3>タイトルを入力</h3>
+          <h2>タイトルを入力</h2>
           <p>ここに説明を入力、ホバーすると出現ここに説明を入力、ホバーすると出現ここに説明を入力、ホバーすると出現ここに説明を入力</p>
         </a>
       </section>
       <section>
         <a href="">
           <div class="image"><img src="https://cdn.pixabay.com/photo/2015/07/17/22/43/student-849822_1280.jpg" alt=""></div>
-          <h3>タイトルを入力</h3>
+          <h2>タイトルを入力</h2>
           <p>ここに説明を入力、ホバーすると出現ここに説明を入力、ホバーすると出現ここに説明を入力、ホバーすると出現ここに説明を入力</p>
         </a>
       </section>
       <section>
         <a href="">
           <div class="image"><img src="https://cdn.pixabay.com/photo/2020/12/09/21/11/women-5818607_1280.jpg" alt=""></div>
-          <h3>タイトルを入力</h3>
+          <h2>タイトルを入力</h2>
           <p>ここに説明を入力、ホバーすると出現ここに説明を入力、ホバーすると出現ここに説明を入力、ホバーすると出現ここに説明を入力</p>
         </a>
       </section>
     </div>
   </article>
-</body>
 
 <!-- 参考サイト
 https://demo.tcd-theme.com/tcd080/ -->
-
-</html>
