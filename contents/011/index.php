1daff4958b87364ab4451ae78db676a73e985ab9
yoshikawa1240 <taka.scholes18@gmail.com>
2021-09-12 10:09:05 +0900

    fix contents_011
    コンテンツ＿011のスライダーを修正

--- contents/011/index.php
+++ contents/011/index.php
@@ -1,24 +1,15 @@
-<!DOCTYPE html>
-<html lang="ja">
+<link rel="stylesheet" href="<?php echo $home_url; ?>/web_parts_ty/contents/011/css/style.min.css" );>
+<link rel="stylesheet" href="<?php echo $home_url; ?>/web_parts_ty/contents/011/css/reset.min.css" );>
+<!-- slick.css -->
+<link rel="stylesheet" href="<?php echo $home_url; ?>/web_parts_ty/contents/011/css/slick.css" );>
+<!-- jQuery_CDN -->
+<script src="https://code.jquery.com/jquery-3.6.0.js"integrity="sha256-H+K7U5CnXl1h5ywQfKtSj8PCmoN9aaq30gDh27Xc0jk=" crossorigin="anonymous"></script>
+<!-- slick.js -->
+<script src="<?php echo $home_url; ?>/web_parts_ty/contents/005/js/slick.min.js" );></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/babel-standalone/6.26.0/babel.min.js"></script>
+<!-- base.js -->
+<script src="<?php echo $home_url; ?>/web_parts_ty/contents/011/js/base.js" );></script>
 
-<head>
-  <meta charset="UTF-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <title>cont_parts_y_011</title>
-  <link rel="stylesheet" href="./css/reset.css">
-  <link rel="stylesheet" href="./css/style.css">
-  <!-- FontAwesome -->
-  <link href="https://use.fontawesome.com/releases/v5.6.1/css/all.css" rel="stylesheet">
-  <!-- GoogleFont -->
-  <link rel="preconnect" href="https://fonts.gstatic.com">
-  <link href="https://fonts.googleapis.com/css2?family=Noto+Serif+JP:wght@200;400;500;700;900&display=swap"
-    rel="stylesheet">
-  <!-- slick.css_cdn -->
-  <link rel="stylesheet" type="text/css" href="https://cdnjs.cloudflare.com/ajax/libs/slick-carousel/1.9.0/slick.css" />
-  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/slick-carousel/1.9.0/slick-theme.css">
-</head>
-
-<body>
   <!-------------- 
     cont_parts_y_011
   ---------------->
@@ -27,14 +18,14 @@
       <section>
         <div class="main_image"><img src="https://cdn.pixabay.com/photo/2016/08/09/21/54/lake-1581879_1280.jpg" alt="">
         </div>
-        <h3>タイトルを入力</h3>
-        <div class="slider">
+        <h2>タイトルを入力</h2>
+        <div class="cont_parts_y_011_slider">
           <a href="">
             <div class="sub_image"><img
                 src="https://cdn.pixabay.com/photo/2016/10/16/13/44/young-woman-1745173_1280.jpg" alt=""></div>
             <div class="inner">
               <p>2021.01.01</p>
-              <h4>タイトルをここに入力タイトルをここに入力タイトルをここに入力</h4>
+              <h3>タイトルをここに入力タイトルをここに入力タイトルをここに入力</h3>
             </div>
           </a>
           <a href="">
@@ -42,7 +33,7 @@
                 src="https://cdn.pixabay.com/photo/2016/10/16/13/44/young-woman-1745173_1280.jpg" alt=""></div>
             <div class="inner">
               <p>2021.01.01</p>
-              <h4>タイトルをここに入力タイトルをここに入力タイトルをここに入力</h4>
+              <h3>タイトルをここに入力タイトルをここに入力タイトルをここに入力</h3>
             </div>
           </a>
           <a href="">
@@ -50,7 +41,7 @@
                 src="https://cdn.pixabay.com/photo/2016/10/16/13/44/young-woman-1745173_1280.jpg" alt=""></div>
             <div class="inner">
               <p>2021.01.01</p>
-              <h4>タイトルをここに入力タイトルをここに入力タイトルをここに入力</h4>
+              <h3>タイトルをここに入力タイトルをここに入力タイトルをここに入力</h3>
             </div>
           </a>
         </div>
@@ -59,14 +50,14 @@
       <section>
         <div class="main_image"><img src="https://cdn.pixabay.com/photo/2016/08/09/21/54/lake-1581879_1280.jpg" alt="">
         </div>
-        <h3>タイトルを入力</h3>
-        <div class="slider">
+        <h2>タイトルを入力</h2>
+        <div class="cont_parts_y_011_slider">
           <a href="">
             <div class="sub_image"><img src="https://cdn.pixabay.com/photo/2016/01/19/14/56/woman-1149088_1280.jpg"
                 alt=""></div>
             <div class="inner">
               <p>2021.01.01</p>
-              <h4>タイトルをここに入力タイトルをここに入力タイトルをここに入力</h4>
+              <h3>タイトルをここに入力タイトルをここに入力タイトルをここに入力</h3>
             </div>
           </a>
           <a href="">
@@ -74,7 +65,7 @@
                 alt=""></div>
             <div class="inner">
               <p>2021.01.01</p>
-              <h4>タイトルをここに入力タイトルをここに入力タイトルをここに入力</h4>
+              <h3>タイトルをここに入力タイトルをここに入力タイトルをここに入力</h3>
             </div>
           </a>
           <a href="">
@@ -82,7 +73,7 @@
                 alt=""></div>
             <div class="inner">
               <p>2021.01.01</p>
-              <h4>タイトルをここに入力タイトルをここに入力タイトルをここに入力</h4>
+              <h3>タイトルをここに入力タイトルをここに入力タイトルをここに入力</h3>
             </div>
           </a>
         </div>
@@ -90,30 +81,3 @@
 
     </div>
   </article>
-
-  <script type="text/javascript" src="https://code.jquery.com/jquery-1.11.0.min.js"></script>
-  <script type="text/javascript"
-    src="https://cdnjs.cloudflare.com/ajax/libs/slick-carousel/1.9.0/slick.min.js"></script>
-
-
-  <script>
-    $(function () {
-      $('.slider').slick({
-        autoplay: true,
-        // 自動再生
-        autoplaySpeed: 5000,
-        // 自動再生のスライド切り替えまでの時間をミリ秒で設定。
-        slidesToShow: 1,
-        // 表示するスライド数を設定。
-        slidesToScroll: 1,
-        // スクロールするスライド数を設定。
-      });
-    });
-  </script>
-
-</body>
-
-<!-- 参考サイト
-https://demo.tcd-theme.com/tcd080/ -->
-
-</html>
---
+++ contents/011/js/base.js
@@ -0,0 +1,12 @@
+$(function () {
+  $('.cont_parts_y_011_slider').slick({
+    autoplay: true,
+    // 自動再生
+    autoplaySpeed: 5000,
+    // 自動再生のスライド切り替えまでの時間をミリ秒で設定。
+    slidesToShow: 1,
+    // 表示するスライド数を設定。
+    slidesToScroll: 1,
+    // スクロールするスライド数を設定。
+  });
+});
