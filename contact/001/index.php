97ccaaa0aec5c2792c44d5b12e55e4d52676e954
yoshikawa1240 <taka.scholes18@gmail.com>
2021-09-15 20:35:33 +0900

    fix contact_001
    コンタクト＿001の修正

--- contact/001/index.php
+++ contact/001/index.php
@@ -1,27 +1,6 @@
-<!DOCTYPE html>
-<html lang="ja">
+<link rel="stylesheet" href="<?php echo $home_url; ?>/web_parts_ty/contact/001/css/style.min.css" );>
+<link rel="stylesheet" href="<?php echo $home_url; ?>/web_parts_ty/contact/001/css/reset.min.css" );>
 
-<head>
-  <meta charset="UTF-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <title>contact_ty_001</title>
-  <link rel="stylesheet" href="./css/reset.css">
-  <link rel="stylesheet" href="./css/style.css">
-  <!-- FontAwesome -->
-  <link href="https://use.fontawesome.com/releases/v5.6.1/css/all.css" rel="stylesheet">
-  <!-- GoogleFont -->
-  <link rel="preconnect" href="https://fonts.gstatic.com">
-  <link href="https://fonts.googleapis.com/css2?family=Noto+Serif+JP:wght@200;400;500;700;900&display=swap"
-    rel="stylesheet">
-  <!-- slick.css_cdn -->
-  <link rel="stylesheet" type="text/css" href="https://cdnjs.cloudflare.com/ajax/libs/slick-carousel/1.9.0/slick.css" />
-  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/slick-carousel/1.9.0/slick-theme.css">
-</head>
-
-<body>
-  <!-------------- 
-    contact_ty_001
-  ---------------->
   <article class="contact_ty_001">
     <h2>Webサイトに関することなら<br>
       お気軽にご相談ください</h2>
@@ -53,24 +32,3 @@
       <p>平日10:00～18:00まで。セールス目的のお電話はご遠慮ください。</p>
     </div>
   </article>
-
-  <script type="text/javascript" src="https://code.jquery.com/jquery-1.11.0.min.js"></script>
-  <script type="text/javascript"
-    src="https://cdnjs.cloudflare.com/ajax/libs/slick-carousel/1.9.0/slick.min.js"></script>
-
-
-</body>
-
-<script>
-  $(function () {
-    let tabs = $(".tab"); // tabのクラスを全て取得し、変数tabsに配列で定義
-    $(".tab").on("click", function () { // tabをクリックしたらイベント発火
-      $(".active").removeClass("active"); // activeクラスを消す
-      $(this).addClass("active"); // クリックした箇所にactiveクラスを追加
-      const index = tabs.index(this); // クリックした箇所がタブの何番目か判定し、定数indexとして定義
-      $(".news").removeClass("show").eq(index).addClass("show"); // showクラスを消して、contentクラスのindex番目にshowクラスを追加
-    })
-  })
-</script>
-
-</html>
