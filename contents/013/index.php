426ba724559d22b74d4bf3730845231ca8622b27
yoshikawa1240 <taka.scholes18@gmail.com>
2021-09-17 21:01:20 +0900

    create contents_013
    コンテンツ＿013の作成

---
+++ contents/013/index.php
@@ -0,0 +1,83 @@
+<link rel="stylesheet" href="<?php echo $home_url; ?>/web_parts_ty/contents/013/css/style.min.css" );>
+<link rel="stylesheet" href="<?php echo $home_url; ?>/web_parts_ty/contents/013/css/reset.min.css" );>
+<!-- slick.css -->
+<link rel="stylesheet" href="<?php echo $home_url; ?>/web_parts_ty/contents/013/css/slick.css" );>
+<!-- jQuery_CDN -->
+<script src="https://code.jquery.com/jquery-3.6.0.js"integrity="sha256-H+K7U5CnXl1h5ywQfKtSj8PCmoN9aaq30gDh27Xc0jk=" crossorigin="anonymous"></script>
+<!-- slick.js -->
+<script src="<?php echo $home_url; ?>/web_parts_ty/contents/005/js/slick.min.js" );></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/babel-standalone/6.26.0/babel.min.js"></script>
+<!-- base.js -->
+<script src="<?php echo $home_url; ?>/web_parts_ty/contents/013/js/base.js" );></script>
+
+  <!-------------- 
+    cont_ty_013
+  ---------------->
+  <article class="cont_ty_013">
+    <div class="wrapper">
+      <section>
+        <div class="main_image"><img src="https://cdn.pixabay.com/photo/2016/08/09/21/54/lake-1581879_1280.jpg" alt="">
+        </div>
+        <h2>タイトルを入力</h2>
+        <div class="cont_parts_y_011_slider">
+          <a href="">
+            <div class="sub_image"><img
+                src="https://cdn.pixabay.com/photo/2016/10/16/13/44/young-woman-1745173_1280.jpg" alt=""></div>
+            <div class="inner">
+              <p>2021.01.01</p>
+              <h3>タイトルをここに入力タイトルをここに入力タイトルをここに入力</h3>
+            </div>
+          </a>
+          <a href="">
+            <div class="sub_image"><img
+                src="https://cdn.pixabay.com/photo/2016/10/16/13/44/young-woman-1745173_1280.jpg" alt=""></div>
+            <div class="inner">
+              <p>2021.01.01</p>
+              <h3>タイトルをここに入力タイトルをここに入力タイトルをここに入力</h3>
+            </div>
+          </a>
+          <a href="">
+            <div class="sub_image"><img
+                src="https://cdn.pixabay.com/photo/2016/10/16/13/44/young-woman-1745173_1280.jpg" alt=""></div>
+            <div class="inner">
+              <p>2021.01.01</p>
+              <h3>タイトルをここに入力タイトルをここに入力タイトルをここに入力</h3>
+            </div>
+          </a>
+        </div>
+      </section>
+
+      <section>
+        <div class="main_image"><img src="https://cdn.pixabay.com/photo/2016/08/09/21/54/lake-1581879_1280.jpg" alt="">
+        </div>
+        <h2>タイトルを入力</h2>
+        <div class="cont_parts_y_011_slider">
+          <a href="">
+            <div class="sub_image"><img src="https://cdn.pixabay.com/photo/2016/01/19/14/56/woman-1149088_1280.jpg"
+                alt=""></div>
+            <div class="inner">
+              <p>2021.01.01</p>
+              <h3>タイトルをここに入力タイトルをここに入力タイトルをここに入力</h3>
+            </div>
+          </a>
+          <a href="">
+            <div class="sub_image"><img src="https://cdn.pixabay.com/photo/2016/01/19/14/56/woman-1149088_1280.jpg"
+                alt=""></div>
+            <div class="inner">
+              <p>2021.01.01</p>
+              <h3>タイトルをここに入力タイトルをここに入力タイトルをここに入力</h3>
+            </div>
+          </a>
+          <a href="">
+            <div class="sub_image"><img src="https://cdn.pixabay.com/photo/2016/01/19/14/56/woman-1149088_1280.jpg"
+                alt=""></div>
+            <div class="inner">
+              <p>2021.01.01</p>
+              <h3>タイトルをここに入力タイトルをここに入力タイトルをここに入力</h3>
+            </div>
+          </a>
+        </div>
+      </section>
+
+    </div>
+  </article>
