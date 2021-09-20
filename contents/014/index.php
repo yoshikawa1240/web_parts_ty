30e7ca2f00ac683f4f971f16fa8d363dbf2d453f
yoshikawa1240 <taka.scholes18@gmail.com>
2021-09-20 15:13:39 +0900

    add contents_014
    コンテンツ＿014の追加

---
+++ contents/014/index.php
@@ -0,0 +1,42 @@
+<link rel="stylesheet" href="<?php echo $home_url; ?>/web_parts_ty/contents/014/css/style.min.css" );>
+<link rel="stylesheet" href="<?php echo $home_url; ?>/web_parts_ty/contents/014/css/reset.min.css" );>
+
+  <!-------------- 
+    cont_ty_014
+  ---------------->
+  <article class="cont_ty_014">
+    <header>
+      <h1 class="pc">ここにタイトルを入力</h1>
+      <h1 class="sp">ここにタイトルを入力<br>ここにタイトル</h1>
+    </header>
+    <section>
+      <div class="inner">
+        <figure>
+          <img src="https://cdn.pixabay.com/photo/2021/09/07/11/53/car-6603726_1280.jpg" alt="">
+        </figure>
+        <h2>ここにサブタイトルを入力</h2>
+        <p>ここにテキストを入力。ここにテキストを入力。ここにテキストを入力。ここにテキストを入力。ここにテキストを入力。ここにテキストを入力。ここにテキストを入力。</p>
+      </div>
+      <div class="inner">
+        <figure>
+          <img src="https://cdn.pixabay.com/photo/2021/09/04/09/33/house-6597406_1280.jpg" alt="">
+        </figure>
+        <h2>ここにサブタイトルを入力</h2>
+        <p>ここにテキストを入力。ここにテキストを入力。ここにテキストを入力。ここにテキストを入力。ここにテキストを入力。ここにテキストを入力。ここにテキストを入力。</p>
+      </div>
+      <div class="inner">
+        <figure>
+          <img src="https://cdn.pixabay.com/photo/2021/08/01/12/58/beach-6514331_1280.jpg" alt="">
+        </figure>
+        <h2>ここにサブタイトルを入力</h2>
+        <p>ここにテキストを入力。ここにテキストを入力。ここにテキストを入力。ここにテキストを入力。ここにテキストを入力。ここにテキストを入力。ここにテキストを入力。</p>
+      </div>
+      <div class="inner">
+        <figure>
+          <img src="https://cdn.pixabay.com/photo/2021/08/01/12/58/beach-6514331_1280.jpg" alt="">
+        </figure>
+        <h2>ここにサブタイトルを入力</h2>
+        <p>ここにテキストを入力。ここにテキストを入力。ここにテキストを入力。ここにテキストを入力。ここにテキストを入力。ここにテキストを入力。ここにテキストを入力。</p>
+      </div>
+    </section>
+  </article>
