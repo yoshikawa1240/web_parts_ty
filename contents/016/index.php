f29d696c3a633e192f8cea3369143dfcbfe5877f
yoshikawa1240 <taka.scholes18@gmail.com>
2021-09-20 16:28:34 +0900

    add contents_016
    コンテンツ＿016の追加

---
+++ contents/016/index.php
@@ -0,0 +1,56 @@
+<link rel="stylesheet" href="<?php echo $home_url; ?>/web_parts_ty/contents/016/css/reset.min.css" );>
+<link rel="stylesheet" href="<?php echo $home_url; ?>/web_parts_ty/contents/016/css/style.min.css" );>
+
+<!----------------------------------------- 
+  cont_ty_016
+  ------------------------------------------>
+  <article class="cont_ty_016">
+    <header>
+      <h1>〇〇〇〇の6つのメリット</h1>
+      <p>〇〇〇〇の６つのメリットについてご紹介いたします。</p>
+    </header>
+    <section>
+      <div class="inner text_in_1s">
+        <figure>
+          <img src="https://cdn.pixabay.com/photo/2021/09/07/09/23/building-6603394_1280.jpg" alt="">
+        </figure>
+        <h2>タイトルを入力</h2>
+        <p>ここに説明を入力。ここに説明を入力。ここに説明を入力。ここに説明を入力。ここに説明を入力。ここに説明を入力。ここに説明を入力。ここに説明を入力。</p>
+      </div>
+      <div class="inner text_in_1s">
+        <figure>
+          <img src="https://cdn.pixabay.com/photo/2021/09/07/09/23/building-6603394_1280.jpg" alt="">
+        </figure>
+        <h2>タイトルを入力</h2>
+        <p>ここに説明を入力。ここに説明を入力。ここに説明を入力。ここに説明を入力。ここに説明を入力。ここに説明を入力。ここに説明を入力。ここに説明を入力。</p>
+      </div>
+      <div class="inner text_in_1s">
+        <figure>
+          <img src="https://cdn.pixabay.com/photo/2021/09/07/09/23/building-6603394_1280.jpg" alt="">
+        </figure>
+        <h2>タイトルを入力</h2>
+        <p>ここに説明を入力。ここに説明を入力。ここに説明を入力。ここに説明を入力。ここに説明を入力。ここに説明を入力。ここに説明を入力。ここに説明を入力。</p>
+      </div>
+      <div class="inner text_in_1s">
+        <figure>
+          <img src="https://cdn.pixabay.com/photo/2021/09/07/09/23/building-6603394_1280.jpg" alt="">
+        </figure>
+        <h2>タイトルを入力</h2>
+        <p>ここに説明を入力。ここに説明を入力。ここに説明を入力。ここに説明を入力。ここに説明を入力。ここに説明を入力。ここに説明を入力。ここに説明を入力。</p>
+      </div>
+      <div class="inner text_in_1s">
+        <figure>
+          <img src="https://cdn.pixabay.com/photo/2021/09/07/09/23/building-6603394_1280.jpg" alt="">
+        </figure>
+        <h2>タイトルを入力</h2>
+        <p>ここに説明を入力。ここに説明を入力。ここに説明を入力。ここに説明を入力。ここに説明を入力。ここに説明を入力。ここに説明を入力。ここに説明を入力。</p>
+      </div>
+      <div class="inner text_in_1s">
+        <figure>
+          <img src="https://cdn.pixabay.com/photo/2021/09/07/09/23/building-6603394_1280.jpg" alt="">
+        </figure>
+        <h2>タイトルを入力</h2>
+        <p>ここに説明を入力。ここに説明を入力。ここに説明を入力。ここに説明を入力。ここに説明を入力。ここに説明を入力。ここに説明を入力。ここに説明を入力。</p>
+      </div>
+    </section>
+  </article>
