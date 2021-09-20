628ebb3541ceabcfd9c30adee925e0dada25b3e2
yoshikawa1240 <taka.scholes18@gmail.com>
2021-09-20 16:18:20 +0900

    add contents_015
    コンテンツ＿015の追加

---
+++ contents/015/index.php
@@ -0,0 +1,56 @@
+<link rel="stylesheet" href="<?php echo $home_url; ?>/web_parts_ty/contents/015/css/style.min.css" );>
+<link rel="stylesheet" href="<?php echo $home_url; ?>/web_parts_ty/contents/015/css/reset.min.css" );>
+
+  <!----------------------------------------- 
+  top_plan
+  ------------------------------------------>
+  <article class="top_plan">
+    <header>
+      <h1>料金プラン</h1>
+      <p>料金は参考価格となります。個別にお見積りが可能ですので、お気軽にお問い合わせください。</p>
+    </header>
+    <section>
+      <div class="outer">
+        <h2>ライト</h2>
+        <div class="inner">
+          <span>200,000円〜</span>
+          <p>売れるランディングページが欲しい方</p>
+          <ul>
+            <li>1ページLP</li>
+            <li>SEO対策</li>
+            <li>レスポンシブ対応</li>
+            <li>お問い合わせフォーム</li>
+            <li>最短1週間</li>
+          </ul>
+        </div>
+      </div>
+      <div class="outer">
+        <h2>スタンダード</h2>
+        <div class="inner">
+          <span>300,000円〜</span>
+          <p>基本的なページを網羅したサイトが欲しい方</p>
+          <ul>
+            <li>3～5ページ</li>
+            <li>SEO対策</li>
+            <li>レスポンシブ対応</li>
+            <li>お問い合わせフォーム</li>
+            <li>最短2週間</li>
+          </ul>
+        </div>
+      </div>
+      <div class="outer">
+        <h2>プレミアム</h2>
+        <div class="inner">
+          <span>500,000円〜</span>
+          <p>ボリューム感のあるサイトが欲しい方</p>
+          <ul>
+            <li>5ページ〜</li>
+            <li>SEO対策</li>
+            <li>レスポンシブ対応</li>
+            <li>お問い合わせフォーム</li>
+            <li>最短3週間</li>
+          </ul>
+        </div>
+      </div>
+    </section>
+  </article>
