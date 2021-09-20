f6db51dd25f004385b269b2ab2fe8bee6649a5ff
yoshikawa1240 <taka.scholes18@gmail.com>
2021-09-20 16:37:03 +0900

    fix contents_013
    コンテンツ＿013の修正

--- contents/013/index.php
+++ contents/013/index.php
@@ -1,14 +1,5 @@
-<link rel="stylesheet" href="<?php echo $home_url; ?>/web_parts_ty/contents/013/css/style.min.css" );>
 <link rel="stylesheet" href="<?php echo $home_url; ?>/web_parts_ty/contents/013/css/reset.min.css" );>
-<!-- slick.css -->
-<link rel="stylesheet" href="<?php echo $home_url; ?>/web_parts_ty/contents/013/css/slick.css" );>
-<!-- jQuery_CDN -->
-<script src="https://code.jquery.com/jquery-3.6.0.js"integrity="sha256-H+K7U5CnXl1h5ywQfKtSj8PCmoN9aaq30gDh27Xc0jk=" crossorigin="anonymous"></script>
-<!-- slick.js -->
-<script src="<?php echo $home_url; ?>/web_parts_ty/contents/005/js/slick.min.js" );></script>
-<script src="https://cdnjs.cloudflare.com/ajax/libs/babel-standalone/6.26.0/babel.min.js"></script>
-<!-- base.js -->
-<script src="<?php echo $home_url; ?>/web_parts_ty/contents/013/js/base.js" );></script>
+<link rel="stylesheet" href="<?php echo $home_url; ?>/web_parts_ty/contents/013/css/style.min.css" );>
 
   <!-------------- 
     cont_ty_013
